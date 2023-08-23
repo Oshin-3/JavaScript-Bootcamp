fa9fbf5cd2d9ff3e3f508de66a5fbc536dafebec
Oshin-3 <oshin.gawande3@gmail.com>
2023-08-23 19:23:26 +0530

    first commit

---
+++ Assignment-day1/assignment1.js
@@ -0,0 +1,22 @@
+//Assignment 1: Array Operations
+//Objective: Practice adding, removing, and updating values in arrays.
+
+//1. Create an empty array called "fruits".
+let fruits = [];
+
+//2. Add the following fruits to the "fruits" array: "apple", "banana", "orange".
+fruits.push('apple');
+fruits.push('banana');
+fruits.push('orange')
+
+//3. Remove the first fruit from the array.
+fruits.shift();
+
+//4. Add "grape" to the end of the array.
+fruits.push('grape');
+
+//5. Update the second fruit in the array to "pear".
+fruits[1] = 'pear';
+
+//6. Print the final "fruits" array after performing the above operations.
+console.log(fruits);
